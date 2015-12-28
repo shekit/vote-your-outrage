2860f88a56308c77cbbfc59a7e66c7a44e69ec4d
shekit <as7790@nyu.edu>
2015-12-28 13:50:54 +0530

    creating outrage data on server and sending to client on connect

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -11,8 +11,31 @@ $(document).ready(function(){
 	var voteOneSpan = $(".voteOne");
 	var voteTwoSpan = $(".voteTwo");
 
+	var img = $("#outrage-img");  //img to update
+	var option_one = $("#option-one");  // option one text
+	var option_two = $("#option-two");  // option two text
+	var option_one_votes = $("#option-one-votes");   // vote counter for option one
+	var option_two_votes = $("#option-two-votes");   // vote counter for option two
+
+	var slideInterval = 5000;
+	var slideCounter = 0;
+
+	// array containing details of outrages - filled by server
+	var outrages = []
+
 	voteOneSpan.html(voteOneCount);
 	voteTwoSpan.html(voteTwoCount);
+
+	//receive outrage list from server
+	socket.on("data", function(msg){
+		console.log("got outrages")
+
+		//save copy of list in client
+		outrages = msg;
+
+		//start the outrage slideshow
+
+	})
 
 	socket.on("one", function(msg){
 		console.log("voted yes")
@@ -25,4 +48,12 @@ $(document).ready(function(){
 		voteTwoCount++;
 		voteTwoSpan.html(voteTwoCount);
 	})
+
+
+
+	function runSlides(){
+		slideTimer = setInterval(function(){
+			console.log("hello")
+		}, slideInterval)
+	}
 })
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,44 @@ var router = express.Router();
 var socket = require('socket.io');
 var io = socket();
 
+var outrages = [
+		{
+			"id":1,
+			"img":"../public/images/outrage1.jpg",
+			"option_one":"#warOnChristmas",
+			"option_two":"#jstACup",
+			"option_one_votes":0,
+			"option_two_votes":0
+	    },
+	    {
+			"id":2,
+			"img":"../public/images/outrage2.jpg",
+			"option_one":"#idiot",
+			"option_two":"#patriot",
+			"option_one_votes":0,
+			"option_two_votes":0
+	    },
+	    {
+			"id":3,
+			"img":"../public/images/outrage3.jpg",
+			"option_one":"#freetochoose",
+			"option_two":"#bornthisway",
+			"option_one_votes":0,
+			"option_two_votes":0
+	    }
+]
+
+io.on('connection', function(socket){
+	console.log("a user connected");
+
+	//send outrage list to client
+	socket.emit("data", outrages)
+
+	socket.on('disconnect', function(){
+		console.log("user disconnected")
+	})
+})
+
 /* GET home page. */
 router.post('/message', function(req, res, next){
 	// get users number
